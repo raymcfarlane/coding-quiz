dd42ef236d8b0d24b24559b4d795bae69bc43fe7
raymcfarlane <122712555+raymcfarlane@users.noreply.github.com>
2023-06-19 21:18:04 -0400

    updated style and scripts

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,196 +1,106 @@
-var wordBlank = document.querySelector(".word-blanks");
-var win = document.querySelector(".win");
-var lose = document.querySelector(".lose");
-var timerElement = document.querySelector(".timer-count");
-var startButton = document.querySelector(".start-button");
-
-var chosenWord = "";
-var numBlanks = 0;
-var winCounter = 0;
-var loseCounter = 0;
-var isWin = false;
-var timer;
-var timerCount;
-
-// Arrays used to create blanks and letters on screen
-var lettersInChosenWord = [];
-var blanksLetters = [];
-
-// Array of words the user will guess
-var words = ["variable","array", "modulus", "object", "function", "string", "boolean"];
-
-// The init function is called when the page loads 
-function init() {
-  getWins();
-  getlosses();
-}
-
-// The startGame function is called when the start button is clicked
-function startGame() {
-  isWin = false;
-  timerCount = 10;
-  // Prevents start button from being clicked when round is in progress
-  startButton.disabled = true;
-  renderBlanks()
-  startTimer()
-}
-
-// The winGame function is called when the win condition is met
-function winGame() {
-  wordBlank.textContent = "YOU WON!!!🏆 ";
-  winCounter++
-  startButton.disabled = false;
-  setWins()
-}
-
-// The loseGame function is called when timer reaches 0
-function loseGame() {
-  wordBlank.textContent = "GAME OVER";
-  loseCounter++
-  startButton.disabled = false;
-  setLosses()
-}
-
-// The setTimer function starts and stops the timer and triggers winGame() and loseGame()
-function startTimer() {
-  // Sets timer
-  timer = setInterval(function() {
-    timerCount--;
-    timerElement.textContent = timerCount;
-    if (timerCount >= 0) {
-      // Tests if win condition is met
-      if (isWin && timerCount > 0) {
-        // Clears interval and stops timer
+var questions = [
+    {
+      question: "How can you make a numbered list?",
+      choices: ["<ul>", "<list>", "<ol>", "<dl>"],
+      correctAnswer: 2
+    },
+    {
+      question: "Choose the correct HTML element for the largest heading:",
+      choices: ["<head>", "<h6>", "<heading>", "<h1>"],
+      correctAnswer: 3
+    },
+    {
+      question: "What is the correct HTML element for inserting a line break?",
+      choices: ["<br>", "<lb>", "<jk>", "<break>"],
+      correctAnswer: 0
+    },
+    {
+        question: "Which character is used to indicate an end tag?",
+        choices: ["/", "@", "<", "*"],
+        correctAnswer: 0
+      },
+      {
+        question: "How can you make a bulleted list?",
+        choices: ["<dl>", "<list>", "<ul>", "<kk>"],
+        correctAnswer: 2
+      },
+      {
+        question: "Which HTML attribute specifies an alternate text for an image, if the image cannot be displayed?",
+        choices: ["src", "title", "alt", "long"],
+        correctAnswer: 2
+      }
+  ];
+  
+  var currentQuestion = 0;
+  var score = 0;
+  var timer;
+  var timeRemaining = 60; 
+  
+  function displayQuestion() {
+    var questionElement = document.getElementById("question");
+    var choicesElement = document.getElementById("choices");
+    questionElement.innerHTML = "";
+    choicesElement.innerHTML = "";
+    questionElement.innerHTML = questions[currentQuestion].question;
+      for (var i = 0; i < questions[currentQuestion].choices.length; i++) {
+      var choice = document.createElement("input");
+      choice.type = "radio";
+      choice.name = "quizOption";
+      choice.value = i;
+  
+      var label = document.createElement("label");
+      label.appendChild(choice);
+      label.appendChild(document.createTextNode(questions[currentQuestion].choices[i]));
+  
+      choicesElement.appendChild(label);
+    }
+  }
+  // Function for submit button 
+function submitAnswer() {
+    var selectedOption = document.querySelector('input[name="quizOption"]:checked');
+    if (selectedOption) {
+      var answer = parseInt(selectedOption.value);
+      if (answer === questions[currentQuestion].correctAnswer) {
+        score++;
+      }
+      currentQuestion++;
+      if (currentQuestion === questions.length) {
         clearInterval(timer);
-        winGame();
+        document.getElementById("quizContainer").innerHTML = "Quiz Complete! Your score is: " + score + " out of " + questions.length
+      updateHighScore();
+      return;
+    }
+    displayQuestion();
+  }
+}
+// Timer function
+function startTimer() {
+    var timerElement = document.getElementById("timer");
+
+    timerElement.innerHTML = "Time Remaining: " + timeRemaining + "s";
+
+    var timer = setInterval(function () {
+      timeRemaining--;
+      timerElement.innerHTML = "Time Remaining: " + timeRemaining + "s";
+      if (timeRemaining <= 0 || currentQuestion === questions.length) {
+        clearInterval(timer);
+        if (timeRemaining <= 0) {
+          document.getElementById("quizContainer").innerHTML = "Time's up! Your score is: " + score + " out of " + questions.length;
+        } else {
+          document.getElementById("quizContainer").innerHTML = "Quiz Complete! Your score is: " + score + " out of " + questions.length;
+          updateHighScore();
+        }
       }
-    }
-    // Tests if time has run out
-    if (timerCount === 0) {
-      // Clears interval
-      clearInterval(timer);
-      loseGame();
-    }
-  }, 1000);
-}
-
-// Creates blanks on screen
-function renderBlanks() {
-  // Randomly picks word from words array
-  chosenWord = words[Math.floor(Math.random() * words.length)];
-  lettersInChosenWord = chosenWord.split("");
-  numBlanks = lettersInChosenWord.length;
-  blanksLetters = []
-  // Uses loop to push blanks to blankLetters array
-  for (var i = 0; i < numBlanks; i++) {
-    blanksLetters.push("_");
+    }, 1000);
   }
-  // Converts blankLetters array into a string and renders it on the screen
-  wordBlank.textContent = blanksLetters.join(" ")
-}
-
-// Updates win count on screen and sets win count to client storage
-function setWins() {
-  win.textContent = winCounter;
-  localStorage.setItem("winCount", winCounter);
-}
-
-// Updates lose count on screen and sets lose count to client storage
-function setLosses() {
-  lose.textContent = loseCounter;
-  localStorage.setItem("loseCount", loseCounter);
-}
-
-// These functions are used by init
-function getWins() {
-  // Get stored value from client storage, if it exists
-  var storedWins = localStorage.getItem("winCount");
-  // If stored value doesn't exist, set counter to 0
-  if (storedWins === null) {
-    winCounter = 0;
-  } else {
-    // If a value is retrieved from client storage set the winCounter to that value
-    winCounter = storedWins;
+// Update the high score
+function updateHighScore() {
+    highScore = Math.max(score, highScore);
+    document.getElementById("highScore").textContent = highScore;
   }
-  //Render win count to page
-  win.textContent = winCounter;
-}
+  
+  document.getElementById("submitBtn").addEventListener("click", submitAnswer);
+  
 
-function getlosses() {
-  var storedLosses = localStorage.getItem("loseCount");
-  if (storedLosses === null) {
-    loseCounter = 0;
-  } else {
-    loseCounter = storedLosses;
-  }
-  lose.textContent = loseCounter;
-}
-
-function checkWin() {
-  // If the word equals the blankLetters array when converted to string, set isWin to true
-  if (chosenWord === blanksLetters.join("")) {
-    // This value is used in the timer function to test if win condition is met
-    isWin = true;
-  }
-}
-
-// Tests if guessed letter is in word and renders it to the screen.
-function checkLetters(letter) {
-  var letterInWord = false;
-  for (var i = 0; i < numBlanks; i++) {
-    if (chosenWord[i] === letter) {
-      letterInWord = true;
-    }
-  }
-  if (letterInWord) {
-    for (var j = 0; j < numBlanks; j++) {
-      if (chosenWord[j] === letter) {
-        blanksLetters[j] = letter;
-      }
-    }
-    wordBlank.textContent = blanksLetters.join(" ");
-  }
-}
-
-// Attach event listener to document to listen for key event
-document.addEventListener("keydown", function(event) {
-  // If the count is zero, exit function
-  if (timerCount === 0) {
-    return;
-  }
-  // Convert all keys to lower case
-  var key = event.key.toLowerCase();
-  var alphabetNumericCharacters = "abcdefghijklmnopqrstuvwxyz0123456789 ".split("");
-  // Test if key pushed is letter
-  if (alphabetNumericCharacters.includes(key)) {
-    var letterGuessed = event.key;
-    checkLetters(letterGuessed)
-    checkWin();
-  }
-});
-
-// Attach event listener to start button to call startGame function on click
-startButton.addEventListener("click", startGame);
-
-// Calls init() so that it fires when page opened
-init();
-
-// Bonus: Add reset button
-var resetButton = document.querySelector(".reset-button");
-
-function resetGame() {
-  // Resets win and loss counts
-  winCounter = 0;
-  loseCounter = 0;
-  // Renders win and loss counts and sets them into client storage
-  setWins()
-  setLosses()
-}
-// Attaches event listener to button
-resetButton.addEventListener("click", resetGame);
-
-
-
-
-
-
+  displayQuestion();
+  startTimer();
--- readme.md
+++ readme.md
@@ -9,8 +9,7 @@
 
  ## Description 
 The purpose of this homework challenge was to create a quiz for a user on Javascript.
-
-I worked on the html file. I created the buttons for the quiz. I did not complete all of the funtionality for the site to be able to take the quiz and go through the questions. I plan on going back to complete the functionality once I understand how event listeners and functions work together more. 
+I updated the quiz code functionality and styling now that I have an understanding of html and javascript. 
 
 ## Usage
  - Images:
